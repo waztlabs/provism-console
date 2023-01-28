489a07ef0e99fa68a7b71bdecd6430ffa159760e
Maythiwat Chomchuen <maythiwat.chom@gmail.com>
2023-01-28 18:01:58 +0700

    refactor: format

--- src/index.ts
+++ src/index.ts
@@ -1,145 +1,146 @@
-import tls, { TLSSocket } from 'tls';
-import crypto from 'crypto';
-import { WebSocket } from 'ws';
+import tls, { TLSSocket } from 'tls'
+import crypto from 'crypto'
+import { WebSocket } from 'ws'
 
-import fastify, { FastifyRequest } from 'fastify';
-import fastifyWebsocket from 'fastify-websocket';
-import fastifyCors from 'fastify-cors';
+import fastify, { FastifyRequest } from 'fastify'
+import fastifyWebsocket from 'fastify-websocket'
+import fastifyCors from 'fastify-cors'
 
-import dotenv from 'dotenv';
-dotenv.config();
-
-////////////////////////////////////////////////
-
-const app = fastify({ logger: false });
-app.register(fastifyCors);
-app.register(fastifyWebsocket);
+import dotenv from 'dotenv'
+dotenv.config()
 
 ////////////////////////////////////////////////
 
-const generateId = () => crypto.randomBytes(16).toString('hex');
+const app = fastify({ logger: false })
+app.register(fastifyCors)
+app.register(fastifyWebsocket)
+
+////////////////////////////////////////////////
+
+const generateId = () => crypto.randomBytes(16).toString('hex')
 
 interface CachedConsole {
-    uuid: string
-    socket?: TLSSocket
-    isConnected?: boolean
-    hostname: string
-    port: number
-    consolePath: string
+  uuid: string
+  socket?: TLSSocket
+  isConnected?: boolean
+  hostname: string
+  port: number
+  consolePath: string
 }
 
-const vConsoles: CachedConsole[] = [];
+const vConsoles: CachedConsole[] = []
 
 ////////////////////////////////////////////////
 
 const handleConsole = (vConsole: CachedConsole, socket: TLSSocket, ws: WebSocket) => {
-    let dataRecv = '';
-    let detectHttp = false;
+  let dataRecv = ''
+  let detectHttp = false
 
-    socket.on('end', () => {
-        console.log('### Socket End.');
-        ws.close();
-    });
+  socket.on('end', () => {
+    console.log('### Socket End.')
+    ws.close()
+  })
 
-    socket.on('data', (data) => {
-        // console.log('<<H', Buffer.from(data).toString('hex'));
-        // console.log('<<S', JSON.stringify([data.toString()]));
+  socket.on('data', (data) => {
+    // console.log('<<H', Buffer.from(data).toString('hex'))
+    // console.log('<<S', JSON.stringify([data.toString()]))
 
-        if (!detectHttp) {
-            console.log('<<S', JSON.stringify([data.toString()]));
-            dataRecv += data.toString();
-            if (dataRecv.includes('HTTP/1.1 200 OK') && dataRecv.includes('\r\n\r\n')) {
-                detectHttp = true;
-                console.log('### Detected HTTP', JSON.stringify([dataRecv]));
-            }
-            return;
-        }
+    if (!detectHttp) {
+      console.log('<<S', JSON.stringify([data.toString()]))
+      dataRecv += data.toString()
+      if (dataRecv.includes('HTTP/1.1 200 OK') && dataRecv.includes('\r\n\r\n')) {
+        detectHttp = true
+        console.log('### Detected HTTP', JSON.stringify([dataRecv]))
+      }
+      return
+    }
 
-        ws.send(data);
-    });
+    ws.send(data)
+  })
 
-    ws.on('message', (data) => {
-        // console.log('S>>', JSON.stringify([data.toString()]));
-        socket.write(data as Uint8Array);
-    });
+  ws.on('message', (data) => {
+    // console.log('S>>', JSON.stringify([data.toString()]))
+    socket.write(data as Uint8Array)
+  })
 
-    ws.on('close', () => {
-        socket.destroy();
-    });
+  ws.on('close', () => {
+    socket.destroy()
+  })
 
-    socket.write([
-        `CONNECT ${vConsole.consolePath} HTTP/1.0`,
-        `Host: ${vConsole.hostname}`,
-        // `Cookie: session_id=OpaqueRef:XXX`,
-        '', ''
-    ].join('\r\n'));
+  socket.write([
+    `CONNECT ${vConsole.consolePath} HTTP/1.0`,
+    `Host: ${vConsole.hostname}`,
+    // `Cookie: session_id=OpaqueRef:XXX`,
+    '', ''
+  ].join('\r\n'))
 }
 
 ////////////////////////////////////////////////
 
 app.get('/', function (request, reply) {
-    reply.send('Welcome to Provism Console');
-});
+  reply.send('Welcome to Provism Console')
+})
 
 app.get('/list', function (request, reply) {
-    reply.send(vConsoles);
-});
+  reply.send(vConsoles)
+})
 
 app.get('/clear', function (request, reply) {
-    vConsoles.splice(0, vConsoles.length);
-    reply.send(vConsoles);
-});
+  vConsoles.splice(0, vConsoles.length)
+  reply.send(vConsoles)
+})
 
 app.get('/add', function (request: FastifyRequest<{ Querystring: { host: string, port: string, path: string } }>, reply) {
-    if (!request.query.host || !request.query.port || !request.query.path) {
-        reply.status(400).send('error: host, port, path is required.');
-        return;
-    }
+  if (!request.query.host || !request.query.port || !request.query.path) {
+    reply.status(400).send('error: host, port, path is required.')
+    return
+  }
 
-    let vConsole: CachedConsole = {
-        uuid: generateId(),
-        hostname: request.query.host,
-        port: parseInt(request.query.port),
-        consolePath: request.query.path,
-    }
-    vConsoles.push(vConsole);
+  let vConsole: CachedConsole = {
+    uuid: generateId(),
+    hostname: request.query.host,
+    port: parseInt(request.query.port),
+    consolePath: request.query.path,
+  }
 
-    let vncLiteCode = Buffer.from(JSON.stringify({
-        host: process.env.CONSOLE_HOST,
-        port: process.env.CONSOLE_PORT,
-        path: '/console',
-        uuid: vConsole.uuid
-    })).toString('base64');
+  vConsoles.push(vConsole)
 
-    reply.status(200).send({
-        uuid: vConsole.uuid,
-        code: vncLiteCode
-    });
-});
+  let vncLiteCode = Buffer.from(JSON.stringify({
+    host: process.env.CONSOLE_HOST,
+    port: process.env.CONSOLE_PORT,
+    path: '/console',
+    uuid: vConsole.uuid
+  })).toString('base64')
+
+  reply.status(200).send({
+    uuid: vConsole.uuid,
+    code: vncLiteCode
+  })
+})
 
 app.get('/console', { websocket: true }, (connection, request: FastifyRequest<{ Querystring: { uuid: string } }>) => {
-    const ws = connection.socket;
-    const vConsole = vConsoles.find(c => c.uuid == request.query.uuid);
+  const ws = connection.socket
+  const vConsole = vConsoles.find(c => c.uuid == request.query.uuid)
 
-    if (!request.query.uuid || !vConsole) {
-        ws.close(1003, 'invalid console uuid');
-        return;
-    }
+  if (!request.query.uuid || !vConsole) {
+    ws.close(1003, 'invalid console uuid')
+    return
+  }
 
-    try {
-        const socket = tls.connect({
-            host: vConsole.hostname,
-            port: 443,
-            rejectUnauthorized: false
-        });
-        handleConsole(vConsole, socket, ws);
-    }catch(e) {
-        console.log(e);
-        ws.close(1001, 'console transport error');
-        return;
-    }
-});
+  try {
+    const socket = tls.connect({
+      host: vConsole.hostname,
+      port: 443,
+      rejectUnauthorized: false
+    })
+    handleConsole(vConsole, socket, ws)
+  } catch (e) {
+    console.log(e)
+    ws.close(1001, 'console transport error')
+    return
+  }
+})
 
 ////////////////////////////////////////////////
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
